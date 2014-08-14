8a24964f8a8ed3f905c0f189f8a7b2a87ad16f08
Ivan Gris <ivangris4@gmail.com>
2014-08-13 19:09:01 -0600

    Home page and images

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -10,8 +10,17 @@
      <!--property start-->
     <div class="property gray-bg">
         <div class="container">
+            <p>Find out where the party is at. Go out tonight! Search where you want to go, when you want to go, quick and easy with Digital Venue!</p>
             <div class="row">
-
+                <!-- Columns are always 50% wide, on mobile and desktop -->
+                <div class="col-xs-6"><img src="resources/img/date.png" height="128" width="128" class="img-thumbnail"></div>
+                <div class="col-xs-6"><img src="resources/img/event.png" height="128" width="128" class="img-thumbnail"></div>
+                <div class="col-xs-6"><p>Date</p></div>
+                <div class="col-xs-6"><p>Event</p></div>
+                <div class="col-xs-6"><img src="resources/img/venue.png" height="128" width="128" class="img-thumbnail"></div>
+                <div class="col-xs-6"><img src="resources/img/music.png" height="128" width="128" class="img-thumbnail"></div>
+                <div class="col-xs-6"><p>Venue</p></div>
+                <div class="col-xs-6"><p>Music</p></div> 
             </div>
         </div>
     </div>
